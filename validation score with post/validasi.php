bdfa418b50ecffa70c3b134f9f83fa40044a3a9e
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-02-20 18:30:40 +0700

    Add files via upload

---
+++ validation score with post/validasi.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<h1>STATUS KELULUSAN MAHASISWA</h1>
+
+
+Mahasiswa dengan nama <?php echo $_POST["nama"]; ?>
+ NIM <?php echo $_POST["nim"]; ?>
+berhasil menyelsaikan s1 dengan predikat <?php echo $_POST["status"]; ?>
+
+</body>
+</html> 
